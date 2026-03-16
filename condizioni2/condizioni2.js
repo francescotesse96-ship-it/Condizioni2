6c3a02432e111af4d9838f6b4e2fb63b98005b14
francescotesse96-ship-it <francesco.tesse96@gmail.com>
2026-03-16 17:32:17 +0100

    esercizio corretto

--- condizioni2/condizioni2.js
+++ condizioni2/condizioni2.js
@@ -1,52 +1,52 @@
-// let temperatura=Number(prompt (`inserisci la temperatura`));
-
-
-// if ( temperatura < 20 ){
-
-//     console.log ( `non ci sono più le mezze stagioni` );
-
-// }else if ( temperatura >= 30 ){
-//     console.log ( `lu mare, lu sole, lu ientu` );
-    
-// }else if ( temperatura < 30 ){
-//     console.log( `mi dia una peroni ghiacciata` );
-    
-// }else if ( temperatura < 0 ){
-//     console.log( `non è tanto il freddo quanto l'umidità`);
-    
-// }else if( temperatura < -10 ){
-//     console.log( `copriti...ancora ti raffreddi` );
-    
-// }
-
-
 let temperatura=Number(prompt (`inserisci la temperatura`));
 
-let descrizione;
-
-switch (true) {
-
-    case temperatura <= -10:
-        descrizione = `copriti...ancora ti raffreddi`;
-         break;
-
- case temperatura <= 0:
-        descrizione = `non è tanto il freddo quanto l'umidità`;
-         break;
+if( temperatura < -10 ){
+    console.log( `copriti...ancora ti raffreddi` )
 
 
-    case temperatura < 20:
-        descrizione = `non ci sono più le mezze stagioni`;
-         break;
+} else if ( temperatura < 0 ){
+    console.log( `non è tanto il freddo quanto l'umidità`);
+    
+}else if ( temperatura < 20 ){
+    console.log(`non ci sono più le mezze stagioni` );
+    
+}else if ( temperatura < 30 ){
+    console.log ( `mi dia una peroni ghiacciata` );
+    
+}else if ( temperatura >= 30 ){
 
-       case temperatura >= 30:
-        descrizione = `lu mare, lu sole, lu ientu`;
-         break;
-
-         case temperatura < 30:
-        descrizione = `mi dia una peroni ghiacciata`;
-         break;
-
+    console.log (`lu mare, lu sole, lu ientu` );;
+    
 }
-console.log(descrizione);
+
+
+// let temperatura=Number(prompt (`inserisci la temperatura`));
+
+// let descrizione;
+
+// switch (true) {
+
+//     case temperatura <= -10:
+//         descrizione = `copriti...ancora ti raffreddi`;
+//          break;
+
+//  case temperatura <= 0:
+//         descrizione = `non è tanto il freddo quanto l'umidità`;
+//          break;
+
+
+//     case temperatura < 20:
+//         descrizione = `non ci sono più le mezze stagioni`;
+//          break;
+
+//        case temperatura < 30:
+//         descrizione = `mi dia una peroni ghiacciata`;
+//          break;
+
+//          case temperatura >= 30:
+//         descrizione = `lu mare, lu sole, lu ientu` ;
+//          break;
+
+// }
+// console.log(descrizione);
 
